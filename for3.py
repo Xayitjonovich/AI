783715e2e3e186c1931ce7df8137ac564795974f
Xayitjonovich <89039382+Xayitjonovich@users.noreply.github.com>
2022-02-22 14:43:21 +0500

    Add files via upload

---
+++ for3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+a=int(input("a="))
+b=int(input("b="))
+for i in range(b-1,a,-1):
+    print(i)
+print(b-a-1,"ta son qatnashgan")
+    
+
+
+# In[ ]:
+
+
+
+
